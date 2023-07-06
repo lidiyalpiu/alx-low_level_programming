f13de8a148fc3864bd521a66d4950769bcc58211
lidiyalpiu <ananyalidiya@gmail.com>
2023-07-05 19:15:02 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,7 +23,7 @@ int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 			{
-			if (n1 == n2 || n1 == m2 + 1)
+			if (n1 == n2 || n1 == n2 + 1)
 				return (1);
 			return (0 + comparator(s, n1 + 1, n2 - 1));
 			}
@@ -38,5 +38,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (comparators(s,0, _strlen_recursion(s) - 1));
+	return (comparator(s,0, _strlen_recursion(s) - 1));
 }

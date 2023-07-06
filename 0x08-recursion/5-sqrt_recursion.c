2d59c96fa3efe1fa98d993a6821905bdab61e89f
lidiyalpiu <ananyalidiya@gmail.com>
2023-07-05 18:26:35 -0700

    C - Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,10 +20,12 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int n, int i)
 {
-	int sqrt = i * 1;
+	int sqrt = i * i;
 	if (sqrt > n)
+
 		return (-1);
 	if (sqrt == n)
+
 		return (i);
 	return (_sqrt(n, i + 1));
 }

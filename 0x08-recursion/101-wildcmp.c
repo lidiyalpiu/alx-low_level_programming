b11acd338d3700c6d909754388dee187d791452d
lidiyalpiu <ananyalidiya@gmail.com>
2023-07-05 19:50:20 -0700

    C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,7 +4,7 @@
  * wildcmp - compares two strings
  * @s1: string 1
  * #s2: string 2. it can contains a * as a special character
- * Return: 1 if are identical, 0 if not 
+ * Return: 1 if are identical, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -13,7 +13,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == *s2)
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
 
